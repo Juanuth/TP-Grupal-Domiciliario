0aad2b51b675c498aa824c224dcc6a2ec26061b1
Tomaslevys <141672471+Tomaslevys@users.noreply.github.com>
2024-06-25 14:24:55 -0300

    cambios en los script

--- Assets/scripts/ObjectAdmin.cs
+++ Assets/scripts/ObjectAdmin.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class ObjectAdmin : MonoBehaviour
 {
- 
+    public GameObject[] objetos;
+
+    public int numero;
+    int mínimo = 1;
+    int máximo = 8;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        DeactivateAll();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+   void DeactivateAll();
+    {
+    for (int i = 0; i < objetos.Length; i++)
         
     }
+}
 }
