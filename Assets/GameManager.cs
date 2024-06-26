06bcff9319b64c16df5fa68f89af1f744741c602
Tomaslevys <141672471+Tomaslevys@users.noreply.github.com>
2024-06-26 15:28:55 -0300

    cambios en el script GameManager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject[] derecha;
+    public GameObject[] izquierda;
+    public Text text;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        DeactivateAll();
+        ActivateRandom();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void DeactivateAll() {
+        for(int i = 0; i>derecha.Length; i++)
+        {
+            derecha[i].SetActive(false);
+        }
+        for (int i = 0; i > izquierda.Length; i++)
+        {
+            izquierda[i].SetActive(false);
+        }
+
+    }
+    public void ActivateRandom()
+    {
+        int random = Random.Range(0, derecha.Length);
+        derecha[random].SetActive(true);
+        int random1 = Random.Range(0, izquierda.Length);
+        izquierda[random1].SetActive(true);
+
+    }
+
+
+}
