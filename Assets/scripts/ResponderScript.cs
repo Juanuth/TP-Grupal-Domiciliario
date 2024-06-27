867df46f55bb0cc981ccd29f912e6d38770d528e
Tomaslevys <141672471+Tomaslevys@users.noreply.github.com>
2024-06-27 14:28:29 -0300

    Cree el script ResponderScript

---
+++ Assets/scripts/ResponderScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResponderScript : MonoBehaviour
+{
+    public Text Texto1;
+    public Text Texto2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float Texto3 = System.Convert.ToInt32(Texto1.text) + System.Convert.ToInt32(Texto2);
+
+        if (Texto3 != System.Convert.ToInt32(Texto1.text) + System.Convert.ToInt32(Texto2)) {
+            DeactivateAll();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
