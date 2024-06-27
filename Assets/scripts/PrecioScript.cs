9de8c531bdaed59630ab014ca2fa514cc040ad33
Rochi <163904427+RochiC@users.noreply.github.com>
2024-06-27 10:13:20 -0300

    .

---
+++ Assets/scripts/PrecioScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrecioScript : MonoBehaviour
+{
+    public int precioDerecha;
+    public int precioIzquierda;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
