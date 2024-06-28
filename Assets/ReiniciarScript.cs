c9e8498a8a158a210eeea45e93ea1915cb9ced2b
Juanuth <142501793+Juanuth@users.noreply.github.com>
2024-06-28 08:43:17 -0300

    Ya casi funcionan las pantallas de respuesta

--- Assets/ReiniciarScript.cs
+++ Assets/ReiniciarScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ReiniciarScript : MonoBehaviour
 {
@@ -14,5 +15,10 @@ public class ReiniciarScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void LaunchGameScene()
+    {
+        SceneManager.LoadScene("Juego");
     }
 }
