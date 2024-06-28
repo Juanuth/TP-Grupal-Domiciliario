e9782569cd961126077219578e6837eaa9d13511
Tomaslevys <141672471+Tomaslevys@users.noreply.github.com>
2024-06-27 21:18:34 -0300

    cambios en responder script

--- Assets/scripts/ResponderScript.cs
+++ Assets/scripts/ResponderScript.cs
@@ -7,6 +7,7 @@ public class ResponderScript : MonoBehaviour
 {
     public Text Texto1;
     public Text Texto2;
+    public Text Texto3;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +17,8 @@ public class ResponderScript : MonoBehaviour
    
     public void OnButtonpressed()
     {
-        float Texto3 = System.Convert.ToInt32(Texto1.text) + System.Convert.ToInt32(Texto2);
 
-        if (Texto3 != System.Convert.ToInt32(Texto1.text) + System.Convert.ToInt32(Texto2))
+        if (System.Convert.ToInt32(Texto3.text) != System.Convert.ToInt32(Texto1.text) + System.Convert.ToInt32(Texto2))
         {
             Debug.Log("resultado incorrecto");
         }
